14208f3db9d2b1c634146d05e240e6035d3ba776
Dave Risney <dave@deletethis.net>
2020-02-20 22:01:47 -0800

    Remove unnecessary manifest parts

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,35 +1,77 @@
 this.browser = this.browser || this.chrome;
 
-// browser.runtime.onInstalled.addListener((details) => {
-//   console.log('previousVersion', details.previousVersion)
-// })
-// 
-// browser.browserAction.setBadgeText({
-//   text: `'Allo`
-// })
-// 
-// console.log(`'Allo 'Allo! Event Page for Browser Action`)
+try {
+  browser.contextMenus.create({
+      id: "textie-text-link",
+      title: "Get text link",
+      contexts: ["selection"],
+  });
+} catch (e) {
+  console.error("Error creating context menu: " + e);
+}
 
-browser.contextMenus.create({
-    id: "textie-text-link",
-    title: "Selected text link",
-    contexts: ["selection"],
+browser.browserAction.onClicked.addListener(function (tab) {
+  executeFunctionCall(tab.id, getTextSelection).then(text => {
+    invokeTextSelection(text, tab);
+  });
+});
+
+browser.contextMenus.onClicked.addListener(function(info, tab) {
+  switch (info.menuItemId) {
+    case "textie-text-link":
+      invokeTextSelection(info.selectionText, tab);
+      break;
+  }
 });
 
 function createHashFromText(text) {
   return ":~:text=" + encodeURIComponent(text);
 }
 
-browser.contextMenus.onClicked.addListener(function(info, tab) {
-  switch (info.menuItemId) {
-    case "textie-text-link": {
-        browser.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-          const originalUrl = tabs[0].url;
-          const newUrl = new URL(originalUrl);
-          newUrl.hash = createHashFromText(info.selectedText);
-          browser.tabs.update({ url: newUrl.href });
-        });
-      }
-      break;
-  }
-})
+function getTextSelection() {
+  return window.getSelection().toString();
+}
+
+function navigate(uri) {
+  console.log('navigating ' + location.href + ' to ' + uri);
+  location.href = uri;
+  location.reload();
+}
+
+function makeFunctionCallString(fn, params) {
+  let result = "(" + fn.toString() + ")("
+    + (params || []).map(param => JSON.stringify(param)).join(", ")
+    + ")";
+
+  return result;
+}
+
+function Deferral() {
+  this.resolve = null;
+  this.reject = null;
+  this.promise = new Promise(
+    resolve => { this.resolve = resolve },
+    reject => { this.reject = reject });
+}
+
+function executeFunctionCall(tabId, fn, params) {
+  const code = makeFunctionCallString(fn, params);
+  console.log("executing " + code);
+  const deferral = new Deferral();
+  browser.tabs.executeScript(tabId, { code }, deferral.resolve);
+  return deferral.promise;
+}
+
+function invokeTextSelection(selectionText, tab) {
+  const newUrl = new URL(tab.url);
+  newUrl.hash = createHashFromText(selectionText);
+  console.log("starting update " + tab.id + " to " + newUrl.href);
+
+  browser.tabs.update(tab.id, { url: newUrl.href, active: true }, tab => { 
+    console.log("update finished " + tab.id + " to " + tab.url);
+    if (tab.url !== newUrl.href) {
+      console.log("update failed. try executeScript");
+      executeFunctionCall(tab.id, navigate, [newUrl.href]);
+    }
+  });
+}
